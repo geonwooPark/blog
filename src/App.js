d79798a0709a5ced70459ba35e3cfb50b0fb0370
박건우 <white0581@naver.com>
2023-10-01 16:22:01 +0900

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
